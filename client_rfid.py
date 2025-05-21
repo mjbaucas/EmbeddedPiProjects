57b019bf1f3fc9282a5342c7c1f65d5ec2d46c4c
mjbaucas <mjbaucas@gmail.com>
2025-05-21 15:39:18 -0400

    Fixed indentation

--- client_rfid.py
+++ client_rfid.py
@@ -8,21 +8,21 @@ reader = SimpleMFRC522()
 message = ""
 
 while True:
-	try:
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.connect(("10.12.240.116", 5000))
-		
-		try:
-			id, text = reader.read()
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(("10.12.240.116", 5000))
+        
+        try:
+            id, text = reader.read()
             print(id)
             print(text)
-			s.sendall(bytes(id, "utf-8"))
-			message = s.recv(1024).decode("utf-8")
-		finally:
-			GPIO.cleanup()
-			
-		print(message)
-		s.close()
-	except Exception as msg:
-		print(msg)
-		reset = 0
+            s.sendall(bytes(id, "utf-8"))
+            message = s.recv(1024).decode("utf-8")
+        finally:
+            GPIO.cleanup()
+            
+        print(message)
+        s.close()
+    except Exception as msg:
+        print(msg)
+        reset = 0
